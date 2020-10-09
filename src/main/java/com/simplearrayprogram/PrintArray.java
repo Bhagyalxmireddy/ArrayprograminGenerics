bbc5bacec5dffcfd770b8bd23dacc2749d4543d3
Bhagi <puppyreddy1997@gmail.com>
2020-10-09 09:58:46 +0530

    [Bhagyalaxmi]Added:printing the simple array program

--- src/main/java/com/simplearrayprogram/PrintArray.java
+++ src/main/java/com/simplearrayprogram/PrintArray.java
@@ -1,5 +1,33 @@
 package com.simplearrayprogram;
 
 public class PrintArray {
+    public static void main(String[] args){
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        Double[] doubleArray = { 1.1, 2.2, 3.3,4.4 };
+        Character[] charArray = { 'H', 'E' ,'L','L','O'};
+
+        PrintArray.toPrint(intArray);
+        PrintArray.toPrint(doubleArray);
+        PrintArray.toPrint(charArray);
+    }
+
+    private static void toPrint(Integer[] inputArray) {
+        for(int element : inputArray) {
+            System.out.printf("%s ",element);
+        }
+        System.out.println();
+    }
+    private static void toPrint(Double[] inputArray) {
+        for(double element : inputArray) {
+            System.out.printf("%s ",element);
+        }
+        System.out.println();
+    }
+    private static void toPrint(Character[] inputArray) {
+        for(char element : inputArray) {
+            System.out.printf("%s ",element);
+        }
+        System.out.println();
+    }
 
 }
