97403d0f7beb5234e5c6fd031f6d2c8f25b78dbd
Bhagi <puppyreddy1997@gmail.com>
2020-10-09 11:15:02 +0530

    [Bhagyalaxmi]Added:printing the array using generic class

--- src/main/java/com/simplearrayprogram/PrintArray.java
+++ src/main/java/com/simplearrayprogram/PrintArray.java
@@ -1,16 +1,26 @@
 package com.simplearrayprogram;
 
-public class PrintArray {
+public class PrintArray<T> {
+    T[] myArray;
+    public PrintArray(T[] myArray){
+        this.myArray = myArray;
+    }
 
     public static void main(String[] args){
         Integer[] intArray = {1, 2, 3, 4, 5};
         Double[] doubleArray = { 1.1 , 2.2 , 3.3 , 4.4};
         Character[] CharArray = {'H','E','L','L','O'};
 
-        PrintArray.toPrint(intArray);
-        PrintArray.toPrint(doubleArray);
-        PrintArray.toPrint(CharArray);
+        new PrintArray<Integer>(intArray).toPrint();
+        new PrintArray<Double>(doubleArray).toPrint();
+        new PrintArray<Character>(CharArray).toPrint();
+
     }
+
+    private void toPrint() {
+        toPrint(this.myArray);
+    }
+
     public static <E> void toPrint(E[] inputArray){
         for(E element : inputArray){
             System.out.printf("%s",element);
